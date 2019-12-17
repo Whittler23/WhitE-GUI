450e9686ea4b9b3f1f528d92876b52a0e035d8f1
Szymon Strugala <szymonstrugala23@gmail.com>
2019-12-17 22:34:23 +0100

    Add premake and SFML

---
+++ vendor/premake/premake5.lua
@@ -0,0 +1,84 @@
+printf ""
+printf "For now project configuration is only availible when using Visual Studio"
+printf "Building WhitE project using Premake..."
+printf ""
+
+workspace "WhitE"
+    architecture "x64"
+    location "../../"
+	cppdialect "C++17"
+    
+    configurations {
+        "Debug",
+        "Release"
+    }
+    
+project "WhitE"
+    language "C++"
+    kind "ConsoleApp"
+
+    targetdir "../../bin/bin/%{cfg.buildcfg}/" 
+	objdir "../../bin/obj/%{cfg.buildcfg}/" 
+
+    files {
+         "../../src/**.cpp",
+         "../../src/**.hpp",
+		 "../../src/**.inl"
+    }
+
+    defines { "SFML_STATIC" }
+
+    libdirs { "../../vendor/sfml-2.5.1/lib" }
+
+    includedirs{
+        "../../src",
+        "../../vendor/sfml-2.5.1/include",
+        "../../vendor/entt-3.2.0/src"
+    }
+
+    links{
+        "opengl32.lib",
+        "winmm.lib",
+        "gdi32.lib",
+        "freetype.lib",
+        "flac.lib",
+        "vorbisenc.lib",
+        "vorbisfile.lib",
+        "vorbis.lib",
+        "ogg.lib",
+        "openal32.lib"
+    }
+
+    filter{ "configurations:Debug" }
+        symbols "On"
+
+        links{
+            "sfml-graphics-s-d",
+            "sfml-audio-s-d",
+            "sfml-window-s-d",
+            "sfml-system-s-d"
+        }
+		
+		defines{ "WE_DEBUG" }
+
+    filter{ "configurations:Release" }
+        optimize "On"
+
+        links{
+            "sfml-graphics-s",
+            "sfml-audio-s", 
+            "sfml-window-s",
+            "sfml-system-s"
+        }
+		
+		defines{ "WE_RELEASE" }
+
+		
+    filter{ "system:Windows" }
+        defines{ "WE_WINDOWS" }
+
+    filter{ "system:Unix" }
+        defines{ "WE_LINUX" }
+
+    filter{}
+	
